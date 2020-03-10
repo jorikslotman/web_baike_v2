abf12a61ba7406e7fa358ab0a84af6a731ca59cb
wmdhubs <w_degraaf95@live.nl>
2020-03-11 00:54:56 +0100

    Working About us and solution

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
+(function($) {
+  $(document).ready(function(){
+      var scene = document.getElementById('scene');
+      var parallax = new Parallax(scene);
+
+      $('#over_image').hide();
+      $("#under_image").mouseenter(function() {
+                 $("#over_image").fadeIn('slow');
+      });
+      $("#over_image").mouseleave(function() {
+                 $("#over_image").fadeOut('slow')
+      });
+      $('#fade_in').fadeIn('slow');
+
+
+  });
+
+  window.onload = function() {
+      $('#hide_onload').show();
+        scrolly();
+
+        getElementById("hide_onload").style.display = "inline";
+     };
+
+
+
+
+})(jQuery);
